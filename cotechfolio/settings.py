a51d5a136637fbbb64be5911c35b4d417818537e
Levy Omolo <levy.omolo@student.moringaschool.com>
2024-04-19 10:05:39 +0000

    modify the settings

--- cotechfolio/settings.py
+++ cotechfolio/settings.py
@@ -98,17 +98,23 @@ WSGI_APPLICATION = 'cotechfolio.wsgi.application'
 
 # development
 if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
-       }
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#            'NAME': config('DB_NAME'),
+#            'USER': config('DB_USER'),
+#            'PASSWORD': config('DB_PASSWORD'),
+#            'HOST': config('DB_HOST'),
+#            'PORT': '',
+#        }
        
-   }
+#    }
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # production
 else:
    DATABASES = {
